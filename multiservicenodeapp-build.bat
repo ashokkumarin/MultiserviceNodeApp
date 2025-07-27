c7ceb98ab4a6fb197bb2095b99ec37ddc5791eca
Ashok <aashokkumarin@hotmail.com>
2025-07-27 17:48:11 +0530

    Push initial code

--- multiservicenodeapp-build.bat
+++ multiservicenodeapp-build.bat
@@ -1,8 +1,8 @@
 cd api-gateway
-docker build -t api-gateway .
+docker build -t api-gateway-image .
 cd ../service1
-docker build -t service1 .
+docker build -t service1-image .
 cd ../service2
-docker build -t service2 .
+docker build -t service2-image .
 cd ../
 docker-compose up
